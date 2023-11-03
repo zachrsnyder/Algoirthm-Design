91cd730b0faa7970858c2a4656101be5e5a5c19a
zachrsnyder <118587176+zachrsnyder@users.noreply.github.com>
2023-11-03 00:10:26 -0500

    Delete random_float.py

--- random_float.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def main():
-    f = open("512k.txt", "w")
-    for i in range(0,512001):
-        f.write(str(round(random.uniform(0.0,1024000.0),6)))
-        f.write(", ")
-
-main()
